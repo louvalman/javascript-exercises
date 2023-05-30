226f5b330878381b47cd2f5fa6b6dccf6d4adbff
louvalman <louisdyrhauge@hotmail.com>
2023-05-30 23:29:04 +0200

    finish 04 remove from array with all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+// const removeFromArray = function (array, ...values) {
+//   return array.filter((element) => !values.includes(element));
+// };
 
+const removeFromArray = function (array, ...valuesToRemove) {
+  const newArray = [];
+  array.forEach((element) => {
+    if (!valuesToRemove.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
