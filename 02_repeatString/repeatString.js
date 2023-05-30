6dc759cd3ed6760fb4b76f32f76196681099dad3
louvalman <louisdyrhauge@hotmail.com>
2023-05-30 20:16:52 +0200

    complete all tests from repeatstring (2)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
-let result = '';
-
 const repeatString = function (string, num) {
+  let result = '';
   for (let i = 0; i < num; i++) {
     result += string;
+  }
+  if (num < 0) {
+    result += 'ERROR';
   }
   return result;
 };
