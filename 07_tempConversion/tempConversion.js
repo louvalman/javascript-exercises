bc291b3b1598a8c6486d226af1cac88369fa4bc9
louvalman <louisdyrhauge@hotmail.com>
2023-05-31 17:50:43 +0200

    complete 07 tempconversion exercise git push

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempf) {
+  tempc = (tempf - 32) * (5 / 9);
+  return Number(tempc.toFixed(1)); // because to.Fixed returns a string representation of the temp, we need to wrap it in the Number function to return the result as a string!
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempc) {
+  tempf = (tempc * 9) / 5 + 32;
+  return Number(tempf.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
