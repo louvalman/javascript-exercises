a3922004d1b8d2d8f8bc6318e09f73089b5587ad
louvalman <louisdyrhauge@hotmail.com>
2023-05-31 17:51:56 +0200

    remove from array comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,6 +2,8 @@
 //   return array.filter((element) => !values.includes(element));
 // };
 
+// For every element in our array, we check if valuesToRemove is present - if not, we push it to a new array and return it.
+
 const removeFromArray = function (array, ...valuesToRemove) {
   const newArray = [];
   array.forEach((element) => {
